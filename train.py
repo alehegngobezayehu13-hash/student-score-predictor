c3de63c42c5ad26629872e5b8f9d6cd22bba29ba
alehegngobezayehu13-hash <alehegngobezayehu13@gmail.com>
2026-05-10 12:22:42 +0300

    Trained linear regression model and made predictions

--- train.py
+++ train.py
@@ -2,20 +2,29 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+
 data = pd.read_csv("data/student_scores.csv")
 
 
 X = data[["hours", "sleep", "attendance"]]
 
+
 y = data["score"]
+
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-
 model = LinearRegression()
 
 model.fit(X_train, y_train)
 
-print("Model trained successfully!")
+predictions = model.predict(X_test)
+
+results = pd.DataFrame({
+    "Actual Score": y_test,
+    "Predicted Score": predictions
+})
+
+print(results)
